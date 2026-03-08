7b73800f385b8a2b36824c719c919e7308cf4292
patrick856 <patricknotb2@gmail.com>
2026-03-08 17:36:29 +0200

    navbar alignment fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.navbar')
-  if (window.scrollY > 300) {
+  if (window.scrollY > 172) {
     navbar.classList.add('navbar--scrolled')
   } else {
     navbar.classList.remove('navbar--scrolled')
