1516bf95604364b22d6a39b1689fbfd742fb41b5
patrick856 <patricknotb2@gmail.com>
2026-04-03 15:50:55 +0200

    optimization fixes

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-setTimeout(() => {
-  window.scrollTo({ top: 334, behavior: 'smooth' })
-}, 370)
+  const page = window.location.pathname;
+  const isIndex = page === '/' || page.endsWith('index.html');
 
-/* navbar scroll effect  DO NOT TOUCH */
-if (window.location.pathname === '/' || window.location.pathname.includes('index.html')) {
-window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('.navbar-home')
-  if (window.scrollY > 272) {
-    navbar.classList.add('navbar--scrolled')
-  } else {
-    navbar.classList.remove('navbar--scrolled')
+  if (isIndex) {
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+    }
+    window.scrollTo({ top: 0, behavior: 'instant' })
+
+  setTimeout(() => {
+    window.scrollTo({ top: 334, behavior: 'smooth' })
+  }, 150)
   }
-})
-}
 
-/* hero title fade in on scroll */
-const heroTitle = document.querySelector('.hero__title')
+  /* navbar scroll effect  DO NOT TOUCH */
+  if (window.location.pathname === '/' || window.location.pathname.includes('index.html')) {
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar-home')
+    if (window.scrollY > 272) {
+      navbar.classList.add('navbar--scrolled')
+    } else {
+      navbar.classList.remove('navbar--scrolled')
+    }
+  })
+  }
 
-window.addEventListener('scroll', () => {
-  const scrolled = window.scrollY
-  const maxScroll = 334 // tune this — how many px of scroll before fully visible
+  /* hero title fade in on scroll */
+  const heroTitle = document.querySelector('.hero__title')
 
-  const progress = Math.min(scrolled / maxScroll, 1)
+  window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY
+    const maxScroll = 334 // tune this — how many px of scroll before fully visible
 
-  heroTitle.style.opacity = progress
-  heroTitle.style.transform = `translateY(${(1 - progress) * 30}px)`
-})
+    const progress = Math.min(scrolled / maxScroll, 1)
 
-/* separator contact */
-const separatorDiagonal = document.querySelector('.separator-lines')
+    heroTitle.style.opacity = progress
+    heroTitle.style.transform = `translateY(${(1 - progress) * 30}px)`
+  })
 
-window.addEventListener('scroll', () => {
-  const scrolled = window.scrollY
-  separatorDiagonal.style.backgroundPositionX = (1501 - scrolled * 0.3) + 'px'
-})
+  /* separator contact */
+  const separatorDiagonal = document.querySelector('.separator-lines')
+
+  window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY
+    separatorDiagonal.style.backgroundPositionX = (1501 - scrolled * 0.3) + 'px'
+  })
+});
